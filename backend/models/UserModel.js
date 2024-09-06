73e191f19806c1019df1b3485396327ca4c429b7
fullstackharsh <harshkaralia.code@gmail.com>
2024-09-06 11:07:35 +0530

    added express validation

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -21,4 +21,6 @@ const UserSchema = new Schema({
     }
 })
 
-export default mongoose.model('user', UserSchema)
+const User = mongoose.model('user', UserSchema)
+User.createIndexes()
+export default User
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,28 +1,26 @@
 import express from 'express';
-import UserModel from '../models/UserModel.js';
+import User from '../models/UserModel.js';
+import { body, validationResult } from 'express-validator'
 
 export const authRouter = express.Router();
 
 // create a user using: POST "api/auth"
-authRouter.post('/', async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
-
-        // validation
-        if (!name || !email || !password) {
-            return res.status(400).json({ error: 'Please include name, email, and password.' });
-        }
-
-        // create a new user
-        const user = new UserModel({ name, email, password });
-
-        // save the user to the database
-        await user.save();
-
-        // Return the saved user name and email
-        return res.status(201).json({ message: 'User created successfully', user: { name, email } });
-    } catch (error) {
-        console.error('Error creating user:', error);
-        return res.status(500).json({ error: 'Internal Server Error' });
+authRouter.post('/', [
+    body('name', 'Name must be 3 or more than letters').isLength({ min: 3 }),
+    body('email', 'Enter a valid name').isEmail(),
+    body('password', 'Password must be more that 6 or more than six letters.').isLength({ min: 6 })
+], (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
     }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(User => res.json(User))
+        .catch(err => {
+            console.log(err)
+            res.json({ error: 'Please enter a unique value', message: err.message })
+        })
 });
