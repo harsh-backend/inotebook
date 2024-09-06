12bb9ec9137107a09194df043e4956ca25fc1d51
fullstackharsh <harshkaralia.code@gmail.com>
2024-09-06 12:15:28 +0530

    added minor changes in user(auth) route

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -22,5 +22,4 @@ const UserSchema = new Schema({
 })
 
 const User = mongoose.model('user', UserSchema)
-User.createIndexes()
 export default User
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,23 +4,35 @@ import { body, validationResult } from 'express-validator'
 
 export const authRouter = express.Router();
 
-// create a user using: POST "api/auth"
-authRouter.post('/', [
+// create a user using: POST "api/auth", no login required
+authRouter.post('/createuser', [
     body('name', 'Name must be 3 or more than letters').isLength({ min: 3 }),
     body('email', 'Enter a valid name').isEmail(),
     body('password', 'Password must be more that 6 or more than six letters.').isLength({ min: 6 })
-], (req, res) => {
+], async (req, res) => {
+
+    // if errors, return bad request and errors
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
     }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    }).then(User => res.json(User))
-        .catch(err => {
-            console.log(err)
-            res.json({ error: 'Please enter a unique value', message: err.message })
+
+    // check whether the user with email exists already
+    try {
+        let user = await User.findOne({ email: req.body.email })
+        if (user) {
+            return res.status(400).json({ error: "user with this particular emial already exists" })
+        }
+
+        // creating a new user
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
         })
-});
+        res.json({ user })
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send('there is an error')
+    }
+})
